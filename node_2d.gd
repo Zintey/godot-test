ce5807d753db3fadd90d93f956b6145faffc3507
Zintey <Zintey@users.noreply.github.com>
2025-11-12 18:03:09 +0800

    AAA

---
+++ node_2d.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("你好") # Replace with f body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
